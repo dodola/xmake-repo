4c1d0882a4729086d6c0c1e080d8acf3e9e81aee
dodola <dinophp@gmail.com>
2022-08-03 12:03:52 +0800

    modify lexert14 test

--- packages/l/lexertl14/xmake.lua
+++ packages/l/lexertl14/xmake.lua
@@ -25,59 +25,22 @@ package("lexertl14")
 
     on_test(function (package)
         assert(package:check_cxxsnippets({test = [[
+            
             #include <lexertl/generator.hpp>
             #include <lexertl/lookup.hpp>
             #include <lexertl/memory_file.hpp>
 
-            void test(int argc, char *argv[])
+            void test()
             {
-                enum {eoi, word, ws, newline};
-                lexertl::rules rules_;
-                lexertl::state_machine sm_;
+                typedef lexertl::detail::basic_re_tokeniser_state<char, std::size_t> state;
+                typedef lexertl::detail::basic_re_tokeniser
+                    <char, char, std::size_t> tokeniser;
+                std::string str_("\[\[:xdigit:\]\]");
+                state state_(str_.c_str(), str_.c_str() + str_.size(), 1, 0, std::locale(""), 0);
+                lexertl::detail::basic_re_token<char, char> lhs_;
+                lexertl::detail::basic_re_token<char, char> token_;
 
-                rules_.insert_macro("ws", "[ \t]");
-                rules_.insert_macro("nonws", "[^ \t\n]");
-                rules_.push("{nonws}+", word);
-                rules_.push("{ws}+", ws);
-                rules_.push("\r|\n|\r\n", newline);
-                lexertl::generator::build(rules_, sm_);
-            //    lexertl::debug::dump(sm_, std::cout);
-
-                lexertl::memory_file if_(argc == 2 ? argv[1] :
-                    "include/lexertl/licence_1_0.txt");
-                const char *start_ = if_.data();
-                const char *end_ = start_ + if_.size();
-                lexertl::cmatch results_(start_, end_);
-                int cc = 0, wc = 0, lc = 0;
-
-                do
-                {
-                    lexertl::lookup(sm_, results_);
-
-                    switch (results_.id)
-                    {
-                    case eoi:
-                        break;
-                    case word:
-                        cc += results_.second - results_.first;
-                        ++wc;
-                        break;
-                    case ws:
-                        cc += results_.second - results_.first;
-                        break;
-                    case newline:
-                        ++lc;
-                        cc += results_.second - results_.first;
-                        break;
-                    default:
-                        assert(0);
-                        break;
-                    }
-                } while (results_.id != eoi);
-
-                std::cout << "lines: " << lc << ", words: " << wc <<
-                    ", chars: " << cc << '\n';
-                return 0;
+                tokeniser::next(lhs_, state_, token_);
             }
 
         ]]}, {configs = {languages = "c++14"},
